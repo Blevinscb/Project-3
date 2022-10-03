ef03a927ce8a19e1b5af859a9baa89312ce14eb7
Blevinscb <102980084+Blevinscb@users.noreply.github.com>
2022-10-03 12:55:56 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -24,116 +24,88 @@ namespace Project_3
         /// <param name="bArray"></param>
         /// <param name="item"></param>
         /// <returns>the medianwhen found</returns>
-        
+
         /// <summary>
         /// main method that will use a for loop to read in the contents of a file and store them into an array it will then convert the array to an int array and then will input the contents into the jagged array it will call on other methods to do quicksort and binaray
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            string[] lines = File.ReadAllLines("C:\\Users\\Brian\\Downloads.csv");
-            int[][] jaggedArray = new int[lines.Length][];
-            for (int i = 0; i < jaggedArray.Length; i++)
+            string filepath = "C:\\Users\\Brian\\Downloads";                 //creates the filepath 
+            StreamReader sr = new StreamReader(filepath);                    //creates the stremreader used to read in the material
+            int[][] jaggedArray = new int[20][];                              //creates the jagged array 
+            for (int i = 0; i < 20; i++)                                //for loop to read in elements in the string array
             {
-                string[] sArray = lines[i].Split(',');
-                int[] iArray = Array.ConvertAll(sArray, int.Parse);
+                
+                string[] sArray = sr.ReadLine().Split(',');                                  //will split the lines
+                int[] iArray = new int[sArray.Length];                 //converts the strings to ints
+                int count = 0;                                         // sets count = 0
+                foreach(string line in sArray)                         // will loop through all the contents of the array
+                {
+                    iArray[count] = Int32.Parse(line);                
+                    count++;                                           //increments the count
+                }
                 jaggedArray[i] = iArray;
             }
 
-            QuickSort.Sort(jaggedArray[1]);
-            foreach (int[] I in jaggedArray)
+            Console.WriteLine(String.Join(",", jaggedArray[0]));       //sets the basis for the jagged array
+            Console.WriteLine("This is the time for quick sort");     
+            for(int i = 0; i < 20; i++)                                //for loop to execute the quicksort method until all the items are sorted
             {
-                foreach (int j in I)
-                {
-                    Console.WriteLine(j + "\n");
-                }
+                QuickSort.ArrayS(jaggedArray[i], 0, jaggedArray[i].Length - 1);
+                Console.Write(String.Join(",", jaggedArray[i]));
             }
-            BinarySearch.SearchBinary(jaggedArray[1], 256);
+            Console.WriteLine("Binary Search");
+            for (int i = 0; i < 20; i++)                             //for loop for the binary search to occur
+            {
+                Console.WriteLine(BinarySearch.SearchBinary(jaggedArray[i], 256));
+            }
+
+           
 
         }
     }
-
-
-
-
-
-
-      
-
-
-
+  
     public static class QuickSort
-        {/// <summary>
-        /// uses recursion to call the sorting method for subarrarys
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="array"></param>
-            public static void Sort<T>(T[] array) where T : IComparable
+    {/// <summary>
+    /// will basically go through and sort the numbers using quicksort(quicksort will compare one item to another and swap them accordingly to the right position 
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns>the int array</returns>
+        public static int[] ArrayS(int[] a, int left, int right)
+        {
+            var i = left;
+            var j = right;
+            var pivot = a[left];
+            while(i<=j)           //while loop that will conmtinue as long as the smaller integer is the number on the right
             {
-                Sort(array, 0, array.Length - 1);
-
-            }
-            /// <summary>
-            /// helps specify what part of the array should be sorted
-            /// </summary>
-            /// <typeparam name="T"></typeparam>
-            /// <param name="array"></param>
-            /// <param name="lower"></param>
-            /// <param name="upper"></param>
-            /// <returns></returns>
-            private static T[] Sort<T>(T[] array, int lower, int upper)
-                where T : IComparable
-            {
-                if (lower < upper)
+                while (a[i] < pivot)
                 {
-                    int p = Partition(array, lower, upper);
-                    Sort(array, lower, upper);
-                    Sort(array, lower, p);
-                    Sort(array, p + 1, upper);
+                    i++;
                 }
-                return array;
-            }/// <summary>
-            /// it can pick random values to compare such as the lower and the middle and will compare and sort until sorted
-            /// </summary>
-            /// <typeparam name="T"></typeparam>
-            /// <param name="array"></param>
-            /// <param name="lower"></param>
-            /// <param name="upper"></param>
-            /// <returns></returns>
-            private static int Partition<T>(T[] array, int lower, int upper)
-                where T : IComparable
-            {
-                int i = lower;
-                int j = upper ;
-                T pivot = array[lower];
-                do
+                while (a[j] > pivot)
                 {
-                    while (array[i].CompareTo(pivot) < 0) { i++; }
-                    while (array[i].CompareTo(pivot) > 0) { j--; }
-                    if (i >= j)
-                    {
-                        Swap(array, i, j);
-                    }
+                    j--;
                 }
-                while (i <= j);
-                return j;
+                if(i<=j)
+                {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;        
+                    i++;                   //increments the left
+                    j--;                   //decrements the right
+                }
+            }
+            if (left < j)           //if statement that will execute if the left number is smaller than the right number
+                ArrayS(a, left, j);
+            if (i < right)         //if statement that will execute if the left is smaller than the right
+                ArrayS(a, i, right);
+            return a;
 
-            }
-            /// <summary>
-            /// sets temp = first index and sexond index of the array
-            /// </summary>
-            /// <typeparam name="T"></typeparam>
-            /// <param name="array"></param>
-            /// <param name="first"></param>
-            /// <param name="second"></param>
-            private static void Swap<T>(T[] array, int first, int second)
-            {
-                T temp = array[first];
-                array[first] = array[second];
-                array[second] = temp;
-            }
-
-            }
+        }
+    }
     internal class BinarySearch
     {
         /// <summary>
@@ -144,35 +116,31 @@ namespace Project_3
         /// <returns>the medianwhen found</returns>
         public static int SearchBinary(int[] bArray, int item)
         {
-            int min = 0;
-            int Q = bArray.Length;
-            int max = Q - 1;
-            do
+            int min = 0;                            //sets min = 0
+            int Q = bArray.Length;                  // sets q = the length of the int array
+            int max = Q - 1;                        //sets the max = length -1
+            while (min <= max)                      //while loop that will continue until the value is found
             {
-                int median = (min + max) / 2;
-                if (item > bArray[median])
+                int median = (min + max) / 2;    //creates the median
+                if (bArray[median] == item)      //returns the median incremented if statement is true
                 {
-                    min = median + 1;
+                    return ++median;
                 }
-                else
+                else if (item < bArray[median])   //sets a new max by subtracting one from the median
                 {
                     max = median - 1;
                 }
-                if (bArray[median] == item)
+                else                             //sets the minimum = median + 1
                 {
-                    return median;
-                    Console.WriteLine("Your ietm was found");
-
+                    min = median + 1;
                 }
-            }
-            while (min < max);
-            {
-                return -1;
+
+               
 
             }
-
+            return -1;
         }
     }
-        }
+}
 
 
